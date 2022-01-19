3bfbdc0b86b0c743e3cb233e34023ae24abad913
myoon <myoon@hancom.com>
2022-01-19 14:05:21 +0900

    MetatagEX : 메타태그 추출/분류기
    필요없는 파일 정리
    
    TODO
    ====
    윈도우즈에서 실행시켜야할 배치파일 작성

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/MetatagEX
-# Build directory: D:/MetatagEX/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
