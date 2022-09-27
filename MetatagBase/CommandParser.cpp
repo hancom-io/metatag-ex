217fae84954eac7da5439889ac6c16a2644a6925
jjoeng <jjeong@hancom.com>
2022-09-27 15:11:48 +0900

    [MetatagEX] indent, 라이센스 문구 추가 - tab을 space로 indent 수정 - 라이센스 문구 각 파일에 추가

--- MetatagBase/CommandParser.cpp
+++ MetatagBase/CommandParser.cpp
@@ -1,4 +1,20 @@
-﻿#include <fstream>
+﻿/*
+ * Copyright 2022 Hancom Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fstream>
 #include <vector>
 #include <direct.h>
 #include "CommandParser.h"
@@ -24,14 +40,20 @@ bool CommandParser::ShowHelp()
         if (CommandWord::Help.compare(iter) == 0) {
             char cwd[MAX_PATH];
             _getcwd(cwd, sizeof(cwd));
-            std::string helpFilePath = std::string(cwd) + StringResource::PathSeperator + "MetatagEXHelp.txt";
+            std::string helpFilePath = std::string(cwd) + StringResource::PathSeperator + "..\\MetatagEXHelp.txt";
             std::ifstream readFile(helpFilePath);
             if (readFile.is_open() == true) {
                 std::string s;
                 s.clear();
+				int i = 0;
                 do {
                     readFile.clear();
                     getline(readFile, s);
+
+					if (i < 16) {
+						i++;
+						continue;
+					}
 
                     WCHAR* converted = new WCHAR[s.length() + 1];
                     int nLen = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), s.length() + 1, NULL, NULL);
--- MetatagBase/MetatagEX.cpp
+++ MetatagBase/MetatagEX.cpp
@@ -1,4 +1,20 @@
-﻿#include <regex>
+﻿/*
+ * Copyright 2022 Hancom Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <regex>
 #include <fstream>
 #include <Shlwapi.h>
 #include <io.h>
@@ -6,7 +22,7 @@
 #include "MetatagEX.h"
 #include "opensource/hwpx-owpml-model/OWPMLUtil/HncCtrlChDef.h"
 
-#define	F_OK	0
+#define F_OK 0
 
 void MetatagEX::Initialize()
 {
